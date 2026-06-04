367338c4d86ed78df015317d965d8b0a4b42f7a6
R1zein <egorzaneytb@mail.ru>
2026-06-04 20:16:26 +0300

    push

--- Assets/Scripts/PopUpPanel.cs
+++ Assets/Scripts/PopUpPanel.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopUpPanel : MonoBehaviour
 {
     [SerializeField] private AnimationCurve curve;
     [SerializeField] private float animationDurration;
+    [SerializeField] private Image image;
     private float distance;
     private float progress;
     private float speed = 1;
@@ -22,6 +24,23 @@ public class PopUpPanel : MonoBehaviour
         progress = Mathf.Clamp01(progress);
         lerp = Mathf.Lerp(0, -distance, curve.Evaluate(progress));
         rect.anchoredPosition = new Vector2(lerp, rect.anchoredPosition.y);
+        float angle = 180 * progress;
+        image.transform.localRotation = Quaternion.Euler(0, 0, angle);
+        image.transform.localPosition = Vector3.right*(-30+60*progress);
+        Vector2 elementScreenPos = RectTransformUtility.WorldToScreenPoint(
+            null,
+            image.transform.position
+        );
+        var dis = Vector2.Distance(Input.mousePosition, elementScreenPos);
+        if (dis > 300)
+        {
+            image.color = new Color(1, 1, 1, 0.5f);
+        }
+        else
+        {
+            image.color = new Color(1, 1, 1, 1);
+        }
+
     }
 
     public void Toggle()
